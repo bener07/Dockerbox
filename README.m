7d7717417c4c7aa37723a9c4080b71c595092b6c
bener07 <bernandre07@gmail.com>
2023-09-27 23:05:03 +0100

    readme file

---
+++ README.m
@@ -0,0 +1,20 @@
+# DockerBox
+Dockerbox is a docker managment tool with features like:
+    - User Autentication
+    - Built from scratch php API
+    - Simple routing system
+    - Database interface
+    - VueJS dashboard
+
+## Why?
+There are multiple tools that use docker to manage machines but none of them are with a easy to use interface, this tool aspires to that.
+Our goal is to manage the docker engine with user autentication, with this teachers can manage their students machines and students create and upload code to them.
+
+## Getting Started
+Run docker-compose, if you don't have it then grow up and install it ˋsudo apt install docker-compose docker.ioˋ.
+Then just turn on the project with 
+ˋˋˋ
+docker-compose up -d
+ˋˋˋ
+use -d to run the machines in the background!
+
